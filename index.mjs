36f488b8e1791438a36655fc42fd03661797f313
Matt Quigley <mquigs00@gmail.com>
2025-08-16 12:31:13 -0400

    Updated project to no longer ask for PII.

--- index.mjs
+++ index.mjs
@@ -2,32 +2,12 @@ import {format, parseISO} from 'date-fns';
 import axios from 'axios';
 import {SecretsManagerClient, GetSecretValueCommand} from "@aws-sdk/client-secrets-manager";
 
-/*
-Example JSON received:
-{
-    "formId": "nVNBoEosh2us",
-    "formName": "Middleware Test Form",
-    "submission": {
-        "submissionId": "0adb7f88-ea15-49ab-98e0-cf07a6fb2558",
-        "submissionTime": "2025-07-16T14:14:01.335Z",
-        "lastUpdatedAt": "2025-07-16T14:14:01.335Z",
-        "questions": [
-            {
-                "id": "qC1W",
-                "name": "First Name",
-                "type": "ShortAnswer",
-                "value": "Matt"
-            },
-            ...
-*/
-
 const client = new SecretsManagerClient({region: "us-east-1"});
 
 /**
- * Retrieves a secret value from AWS Secrets Manager
- * 
- * @param {String} secretName
- * @returns the secret value
+ * Retrieves a secret valie from AWS Secrets Manager
+ * @param {String} secretName 
+ * @returns 
  */
 async function getSecret(secretName) {
     const command = new GetSecretValueCommand({
@@ -35,17 +15,16 @@ async function getSecret(secretName) {
     });
     const response = await client.send(command);
 
+    // if a secret was found, return it. Otherwise throw an error
     if (response.SecretString) {
         return response.SecretString;
+    } else {
+        throw new Error("Secret not found for " + secretName);
     }
-
-    const buff = Buffer.from(response.SecretBinary, 'base64');
-    return buff.toString('utf-8');
 }
 
 /**
- * Given the streetAddress, city, state, and zipcode, this function calls the OpenCage forward geocoding API to get the county
- * name
+ * Given an street address, city, state, and zipcode, this function calls the Opencage API and returns the corresponding county name
  * 
  * @param {String} streetAddress 
  * @param {String} city 
@@ -55,11 +34,9 @@ async function getSecret(secretName) {
  */
 async function getCounty(streetAddress, city, state, zipcode) {
     try {
-        // retrieve the OpenCage API key
         const openCageSecret = await getSecret("OpenCageKey");
         const openCageKey = JSON.parse(openCageSecret).OPENCAGE_KEY;
-
-        const addressQuery = `${streetAddress}, ${city}, ${state}, ${zipcode}`; // combine the given address components
+        const addressQuery = `${streetAddress}, ${city}, ${state}, ${zipcode}`;
 
         const response = await axios.get(
             'https://api.opencagedata.com/geocode/v1/json',
@@ -72,10 +49,8 @@ async function getCounty(streetAddress, city, state, zipcode) {
             }
         );
 
-        // retrieve the county value from the response body
         const body = response.data;
         let county = body.results[0].components.county;
-
         return county;
     } catch (error) {
         console.error("Error finding county: ", error);
@@ -83,28 +58,22 @@ async function getCounty(streetAddress, city, state, zipcode) {
 };
 
 /**
- * Checks if there is already a Client record in the Airtable base with the same first name, last name, last four digits of SSN,
- * and date of birth
+ * Given a Fillout.com submission, this function checks if a record already exists in Airtable with that name and choices
  * 
- * @param {String} firstName 
- * @param {String} lastName 
- * @param {String} dateOfBirth 
- * @param {String} lastFourOfSSN 
- * @returns true if the client already exists, false if not
+ * @param {Object} filloutData 
+ * @returns 
  */
-async function clientExists(firstName, lastName, dateOfBirth, lastFourOfSSN) {
+async function matchExists(filloutData) {
     try {
-        // retrieve the Airtable access token from AWS Secrets Manager
         const airtableSecret = await getSecret("AirtableToken");
         const airtableToken = JSON.parse(airtableSecret).AIRTABLE_TOKEN;
 
-        // create our formula to call the Airtable List Records API and filter by formula to only receive records with
-        // the given name, date of birth, and last four of SSN
+        // Airtable allows you to call their listRecords API and include a formul to filter by
         let formula = `AND(
-            {First Name}='${firstName}',
-            {Last Name}='${lastName}',
-            {Birth Date}=DATETIME_PARSE('${dateOfBirth}', 'YYYY-MM-DD'),
-            RIGHT({SSN}, 4)='${lastFourOfSSN}'
+            {First Name}='${filloutData.firstName}',
+            {Favorite Ice Cream Flavor}='${filloutData.favoriteFlavor}',
+            {Jordan or Lebron}='${filloutData.jordanOrLebron}',
+            {Beatles or Stones}='${filloutData.beatlesOrStones}'
         )`;
 
         console.log("Retrieved tokens and created formula. Calling axios.get");
@@ -122,69 +91,45 @@ async function clientExists(firstName, lastName, dateOfBirth, lastFourOfSSN) {
 
         let toReturn = false;
 
-        // if any records were returned, then a client already exists with those attributes
+        // if any matching records were found, return true
         if (response.data.records.length > 0) {
             console.log(response.data.records);
             toReturn = true;
         }
-
         return toReturn;
     } catch (error) {
-        console.error("Error checking if client exists", error);
+        console.error("Error checking if matching record exists", error);
     }
 }
 
 /**
- * Inserts a new Client record into Airtable
+ * Given a Fillout.com submission, this function inserts a new record into Airtable
  * 
- * @param {Object} clientData - all of the client's required data for entry into Airtable
+ * @param {Object} filloutData 
  */
-async function insertClient(clientData) {
+async function insertRecord(filloutData) {
     try {
-        // retrieve Airtable access token from AWS Secrets Manager
+        console.log("Attempting to Insert Client");
         const airtableSecret = await getSecret("AirtableToken");
         const airtableToken = JSON.parse(airtableSecret).AIRTABLE_TOKEN;
 
-        console.log("Retrieved Airtable access token. Attempting to insert Client");
+        //console.log(clientData);
 
         const response = await axios.post(
             'https://api.airtable.com/v0/appsyRmBPaxsZIhaA/tbl4iFS1HK8fArCYg',
             {
                 fields: {
-                    "Fillout Id": clientData.filloutId,
-                    "Rural Area Status": clientData.location.ruralAreaStatus,
-                    "First Name": clientData.basic.firstName,
-                    "Last Name": clientData.basic.lastName,
-                    "Birth Date": clientData.basic.dateOfBirth,
-                    "SSN": clientData.basic.ssn,
-                    "Address": clientData.location.streetAddress,
-                    "City": clientData.location.city,
-                    "State": clientData.location.state,
-                    "Zip": clientData.location.zipcode,
-                    "County": clientData.location.county,
-                    "Primary Phone Number": clientData.contact.phoneNumber,
-                    "Primary Phone Type": clientData.contact.phoneType,
-                    "Best Time To Contact": clientData.contact.bestTimeToContact,
-                    "Email": clientData.contact.emailAddress,
-                    "Preferred Contact Type": clientData.contact.preferredContactMethod,
-                    "Purpose": clientData.housing.visitPurpose,
-                    "How did you hear about us?": clientData.referralSource,
-                    "Ethnicity": clientData.demographics.ethnicity,
-                    "Highest Education": clientData.demographics.highestEducation,
-                    "Household Type": clientData.family.householdType,
-                    "Military Status": clientData.demographics.militaryStatus,
-                    "Gender": clientData.demographics.gender,
-                    "Race": clientData.demographics.race,
-                    "Disabled": clientData.demographics.isDisabled,
-                    "English Proficient": clientData.demographics.englishProficiency,
-                    "Marital Status": clientData.demographics.maritalStatus,
-                    "Preferred Language": clientData.demographics.preferredLanguage,
-                    "Living Status": clientData.housing.currentLivingStatus,
-                    "Cars": clientData.numCars,
-                    "Dependents": clientData.family.numDependents,
-                    "Number of Family members in the house": clientData.family.householdSize,
-                    "Relationship": clientData.relationship,
-                    "Colonias Resident": clientData.coloniasResident
+                    "Fillout Id": filloutData.filloutId,
+                    "First Name": filloutData.firstName,
+                    "Date": filloutData.dateISO,
+                    "Favorite Ice Cream Flavor": filloutData.favoriteFlavor,
+                    "Jordan or Lebron": filloutData.jordanOrLebron,
+                    "Beatles or Stones": filloutData.beatlesOrStones,
+                    "Address": filloutData.streetAddress,
+                    "City": filloutData.city,
+                    "State": filloutData.state,
+                    "Zip": filloutData.zipcode,
+                    "County": filloutData.county,
                 }
             },
             {
@@ -199,7 +144,6 @@ async function insertClient(clientData) {
     } catch (error) {
         console.error("Error inserting client", error);
 
-        // get the detailed error response from Airtable
         if (error.response) {
             console.error("Status Code:", error.response.status);
             console.error("Response Data:", JSON.stringify(error.response.data, null, 2));
@@ -209,148 +153,60 @@ async function insertClient(clientData) {
     }
 };
 
-/**
- * Given a question name ("First Name", "Date of Birth", etc.) the function provides it's value fromt the given question object
- * @param {Array} questions - the array of question objects from the Fillout webhook body
- * @param {String} questionName - the question whose answer is to be retrieved
- * @returns questionObj.value - the client's answer for the given question
- */
-function getAnswerFromQuestion(questions, questionName) {
-    const questionObj = questions.find(question => question.name === questionName); // find the question in the array
-
-    // if the question is not found, throw an error providing which question could not be found
-    if (questionObj === undefined) {
-        throw new Error(`Question ${questionName} not found`);
-    }
-
-    return questionObj.value;
-}
-
 export const handler = async (event) => {
-    let body = JSON.parse(event.body);
-    let questions = body.submission.questions;                      // get the array of question object from the array
+    console.log(JSON.stringify(event, null, 2));
+    const expectedSecret = await getSecret("FILLOUT_SECRET");
+    const incomingSecret = event.headers['x-webhook-secret'];
 
-    const filloutId = body.submission.submissionId;
-
-    const firstName = getAnswerFromQuestion(questions, "First Name");
-    const lastName = getAnswerFromQuestion(questions, "Last Name");
-    const dateOfBirth = getAnswerFromQuestion(questions, "Date of Birth");
-    const lastFourOfSSN = getAnswerFromQuestion(questions, "Last 4 Digits of Social Security Number");
-
-    console.log("Calling client exists");
-
-    // check if the client already exists before proceeding with transforming and loading the data
-    if (await clientExists(firstName, lastName, dateOfBirth, lastFourOfSSN)) {
-        console.log("Client already exists in Airtable");
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify('Client already exists in Airtable'),
+    if (!incomingSecret) {
+        console.log("Webhook secret invalid");
+        return {
+            statusCode: 401,
+            body: JSON.stringify('Incorrect webhook secret'),
         };
-        return response;
+    } else {
+        console.log("Webhook secret verified");
     }
 
-    const paddedSSN = lastFourOfSSN.padStart(9, '0');
-    const address = getAnswerFromQuestion(questions, "Address");
-    const phoneNumber = getAnswerFromQuestion(questions, "Phone Number");
-    const phoneType = getAnswerFromQuestion(questions, "Phone Type");
-    const bestTimeToContact = getAnswerFromQuestion(questions, "Best Time to Contact");
-    const emailAddress = getAnswerFromQuestion(questions, "Email Address");
-    const preferredContactMethod = getAnswerFromQuestion(questions, "Preferred Contact Method");
-    const visitPurpose  = getAnswerFromQuestion(questions, "Visit Purpose");
-    const referralSource = getAnswerFromQuestion(questions, "How did you hear about us?");
-    const ethnicity = getAnswerFromQuestion(questions, "Ethnicity");
-    const highestEducation = getAnswerFromQuestion(questions, "Highest Education");
-    const householdType = getAnswerFromQuestion(questions, "Household Type");
-    const militaryStatus = getAnswerFromQuestion(questions, "Military Status");
-    let gender = getAnswerFromQuestion(questions, "Gender");
-    const race = getAnswerFromQuestion(questions, "Race");
-    const englishProficiency = getAnswerFromQuestion(questions, "Are you proficient in English?");
-    const maritalStatus = getAnswerFromQuestion(questions, "Marital Status");
-    const preferredLanguage = getAnswerFromQuestion(questions, "Preferred Language");
-    const currentLivingStatus = getAnswerFromQuestion(questions, "Current Living Status");
-    const numCars = getAnswerFromQuestion(questions, "Number of Cars");
-    const isDisabled = getAnswerFromQuestion(questions, "Disabled?");
-    const numDependents = getAnswerFromQuestion(questions, "Number of Children Under 18");
-    const householdSize = getAnswerFromQuestion(questions, "Number of People in Household");
-    
-    const dateOfBirthISO = format(parseISO(dateOfBirth), 'yyyy-MM-dd');
+    let body = JSON.parse(event.body);
+    let {filloutId, firstName, date, favoriteFlavor, jordanOrLebron, beatlesOrStones, streetAddress, city, state, zipcode} = body;
 
-    const trimmedPhoneNumber = phoneNumber.substring(2);
-
-    const streetAddress = address.address;
-    const city = address.city;
-    const state = address.state;
-    const zipcode = address.zipCode;
+    const dateISO = format(parseISO(date), 'yyyy-MM-dd');
     const county = await getCounty(streetAddress, city, state, zipcode);
 
-    switch(gender) {
-        case "M":
-            gender = "Male";
-            break;
-        case "F":
-            gender = "Female";
-            break;
-        case "O":
-            gender = "Other";
-            break;
+    console.log("Calling client exists");
+    
+    const filloutData = {
+        filloutId,
+        firstName,
+        favoriteFlavor,
+        jordanOrLebron,
+        beatlesOrStones,
+        dateISO,
+        streetAddress,
+        city,
+        state,
+        zipcode,
+        county
+    };
+
+    // if client
+    if (await matchExists(filloutData)) {
+        console.log("Matching record already exists in Airtable");
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Matching record already exists in Airtable'),
+        };
+        return response;
+    } else {
+        console.log("No matching record found. Inserting record");
+        await insertRecord(filloutData);
     }
-    
-    const clientData = {
-        basic: {
-            firstName,
-            lastName,
-            dateOfBirth: dateOfBirthISO,
-            ssn: paddedSSN
-        },
-        contact: {
-            phoneNumber: trimmedPhoneNumber,
-            phoneType,
-            bestTimeToContact,
-            emailAddress,
-            preferredContactMethod
-        },
-        location: {
-            streetAddress,
-            city,
-            state,
-            zipcode,
-            county,
-            ruralAreaStatus: "Household does not live in a rural area"
-        },
-        demographics: {
-            ethnicity,
-            highestEducation,
-            militaryStatus,
-            gender,
-            race,
-            englishProficiency,
-            maritalStatus,
-            preferredLanguage,
-            isDisabled,
-        },
-        family: {
-            householdType,
-            householdSize,
-            numDependents,
-        },
-        housing: {
-            currentLivingStatus,
-            visitPurpose
-        },
-        numCars,
-        referralSource,
-        coloniasResident: "No",
-        relationship: "Self",
-        filloutId
-    }
-    
-    console.log("Finished transforming data, calling insertClient");
-    
-    await insertClient(clientData);
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('New record added to Airtable!'),
     };
+
     return response;
 }
